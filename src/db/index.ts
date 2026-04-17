676f0dccaf3e736bb23c25f4d26d5006f126d466
Aman1128 <veolvetech@gmail.com>
2026-04-18 00:19:06 +0530

    Fixed Docker and removed run dev

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,8 @@
 import { Pool } from "pg";
 
-const isDocker =
-  process.env.DB_HOST === "db" &&
-  process.env.RUNNING_IN_DOCKER === "true";
 
 const pool = new Pool({
-  host: isDocker ? "db" : "localhost",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
